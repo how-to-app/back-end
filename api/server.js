ae6b8531633651c07d746ceb7219cdf540005ef7
Sheila <s.hopper01110@gmail.com>
2019-03-18 17:42:29 -0400

    Server listening working on login and signup

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const authRouter = require('../auth/auth-routes.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(morgan());
+server.use(express.json());
+
+
+
+//CRUD: 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'SC working' });
+  });
+
+
+
+module.exports = server;
