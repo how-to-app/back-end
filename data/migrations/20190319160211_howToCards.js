d6fb9d8531d335db0f2380146e91d13a49167672
Sheila <s.hopper01110@gmail.com>
2019-03-20 14:31:14 -0400

    image added to migrations and seeds

--- data/migrations/20190319160211_howToCards.js
+++ data/migrations/20190319160211_howToCards.js
@@ -12,7 +12,8 @@ exports.up = function(knex) {
       cards.string('step3', 255);
       cards.string('step4', 255);
       cards.string('step5', 255);
-      cards.string('likes', 128)
+      cards.string('likes', 128);
+      cards.string('image', 255);
     
       
     });
